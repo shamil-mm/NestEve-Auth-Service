768369046d34122a395039124ed83204d1425e30
shamil <shamil880699@gmail.com>
2025-08-09 20:47:31 +0530

    corrected the cookies max age

--- src/constants/cookieOptions.ts
+++ src/constants/cookieOptions.ts
@@ -4,14 +4,16 @@ export const ACCESS_TOKEN_COOKIE_OPTIONS = {
   httpOnly: true,
   secure: config.NODE_ENV !== "development",
   sameSite: "none" as const,
-  maxAge: 15 * 60 * 1000, 
+  maxAge: 15 * 60 , 
+  path: "/",
 }
 
 export const REFRESH_TOKEN_COOKIE_OPTIONS = {
   httpOnly: true,
   secure: config.NODE_ENV !== "development",
   sameSite: "none" as const,
-  maxAge: 7 * 24 * 60 * 60 * 1000, 
+  maxAge: 7 * 24 * 60 * 60 ,
+  path: "/", 
 }
 
 export const CLEAR_TOKEN_COOKIE_OPTIONS={
