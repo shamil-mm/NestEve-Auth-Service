7e4af2160594b97ac8823fd6bfd2422b1ef0d66f
shamil <shamil880699@gmail.com>
2025-08-10 15:56:16 +0530

    set the sameSite laxe for debugging the issue

--- src/constants/cookieOptions.ts
+++ src/constants/cookieOptions.ts
@@ -1,24 +1,22 @@
 import config from "../config/config";
 
+  // secure: config.NODE_ENV !== "development",
 
 export const ACCESS_TOKEN_COOKIE_OPTIONS = {
   httpOnly: true,
-  secure: true,
-  sameSite: "none" as const,
+  sameSite: "lax" as const,
   maxAge: 15 * 60 *1000 , 
   path:'/'
 }
 
 export const REFRESH_TOKEN_COOKIE_OPTIONS = {
   httpOnly: true,
-  secure: true,
-  sameSite: "none" as const,
+  sameSite: "lax" as const,
   maxAge: 7 * 24 * 60 * 60 * 1000,
 }
 
 export const CLEAR_TOKEN_COOKIE_OPTIONS={
   httpOnly: true,
-  secure: true,
-  sameSite: "none" as const,
+  sameSite: "lax" as const,
   maxAge: 0,
 }
