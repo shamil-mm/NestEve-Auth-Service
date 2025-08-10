3e583f7ba45a0c9a724bf3bae407e274c89111bf
shamil <shamil880699@gmail.com>
2025-08-10 16:42:06 +0530

    debugging cors issue

--- src/constants/cookieOptions.ts
+++ src/constants/cookieOptions.ts
@@ -2,21 +2,21 @@ import config from "../config/config";
 
 export const ACCESS_TOKEN_COOKIE_OPTIONS = {
   httpOnly: true,
-  secure: config.NODE_ENV !== "development",
-  sameSite: "none" as const,
+  secure: false,
+  sameSite: "lax" as const,
   maxAge: 15 * 60 *1000 , 
 }
 
 export const REFRESH_TOKEN_COOKIE_OPTIONS = {
   httpOnly: true,
-  secure: config.NODE_ENV !== "development",
-  sameSite: "none" as const,
+  secure: false,
+  sameSite: "lax" as const,
   maxAge: 7 * 24 * 60 * 60 *1000,
 }
 
 export const CLEAR_TOKEN_COOKIE_OPTIONS={
   httpOnly: true,
-  secure: config.NODE_ENV !== "development",
-  sameSite: "none" as const,
+  secure: false,
+  sameSite: "lax" as const,
   maxAge: 0,
 }
