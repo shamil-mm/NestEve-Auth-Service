ef52b75096f742b6ffabf87542e2a3d2833b6beb
shamil <shamil880699@gmail.com>
2025-08-11 15:56:00 +0530

    checking db url

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
 
 import app from "./app";
+import config from "./config/config";
 const PORT = process.env.PORT;
-console.log('Node version:', process.version);
-console.log('Environment:', process.env.NODE_ENV);
+console.log('db url',config.DB_URI)
 
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
