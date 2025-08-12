afd100be737277f6abf5e39d69f7bf21302680bb
shamil <shamil880699@gmail.com>
2025-08-12 20:03:28 +0530

    cors url added

--- src/app.ts
+++ src/app.ts
@@ -23,7 +23,7 @@ app.use((req, res, next) => {
 
 app.use(
   cors({
-    origin: config.FRONTEND_URL,
+    origin: config.FRONTEND_URL?.split(','),
     credentials: true,
   })
 );
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,7 @@
 
 import app from "./app";
-import config from "./config/config";
 const PORT = process.env.PORT;
-console.log('db url',config.DB_URI)
+
 
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
